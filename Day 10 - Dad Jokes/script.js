3c7935deb8b1c724fe5566e2ceb2f6def44749d8
Inkuantum <63226427+Inkuantum@users.noreply.github.com>
2020-12-14 23:05:53 +0100

    Day 10

---
+++ Day 10 - Dad Jokes/script.js
@@ -0,0 +1,27 @@
+const  jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+
+
+    /*fetch('https://icanhazdadjoke.com/', config).then(res => res.json()).then(data => {
+        jokeEl.innerHTML = data.joke
+    })*/
+
+
+}
