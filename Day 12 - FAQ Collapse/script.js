49e060a0e5391bad568765544233bd78f7036b70
Inkuantum <63226427+Inkuantum@users.noreply.github.com>
2020-12-16 22:36:20 +0100

    Day 12

---
+++ Day 12 - FAQ Collapse/script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.faq-toggle')
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentElement.classList.toggle('active')
+    })
+})
