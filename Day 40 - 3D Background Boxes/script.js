925a73436477daa2d1a77a9ad5c3a3c209554fcb
Kirill Inoz <63226427+Inkuantum@users.noreply.github.com>
2021-01-13 19:33:35 +0100

    Day 40

---
+++ Day 40 - 3D Background Boxes/script.js
@@ -0,0 +1,15 @@
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+function createBoxes() {
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++){
+            const box = document.createElement('div')
+            box.classList.add('box')
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+            boxesContainer.appendChild(box)
+        }
+    }
+}
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
